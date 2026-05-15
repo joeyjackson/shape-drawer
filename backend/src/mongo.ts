25fd95ed4a65d3bc5cac502076d9df7450da2e5d
Joey Jackson <joeyjackson4712@gmail.com>
2026-05-15 03:49:47 -0400

    support MONGO_CONNECTION_STRING

--- backend/src/mongo.ts
+++ backend/src/mongo.ts
@@ -1,13 +1,18 @@
 import mongoose from "mongoose";
 
 export const connectToMongo = async () => {
-  const MONGO_HOST = process.env.MONGO_HOST || '127.0.0.1';
-  const MONGO_PORT = process.env.MONGO_PORT || '27017';
-  const MONGO_AUTH_SOURCE = process.env.MONGO_AUTH_SOURCE || 'test';
-  const MONGO_USERNAME = process.env.MONGO_USER || 'testUser'
-  const MONGO_PASSWORD = process.env.MONGO_PASSWORD || '';
-  if (MONGO_PASSWORD === '') {
-    throw "app requires a MONGO_PASSWORD environment variable";
+  let MONGO_CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || '';
+
+  if (MONGO_CONNECTION_STRING === '') {
+    const MONGO_HOST = process.env.MONGO_HOST || '127.0.0.1';
+    const MONGO_PORT = process.env.MONGO_PORT || '27017';
+    const MONGO_AUTH_SOURCE = process.env.MONGO_AUTH_SOURCE || 'test';
+    const MONGO_USERNAME = process.env.MONGO_USER || 'testUser'
+    const MONGO_PASSWORD = process.env.MONGO_PASSWORD || '';
+    if (MONGO_PASSWORD === '') {
+      throw "app requires a MONGO_PASSWORD environment variable";
+    }
+    MONGO_CONNECTION_STRING = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOST}:${MONGO_PORT}/${MONGO_AUTH_SOURCE}?authSource=${MONGO_AUTH_SOURCE}`;
   }
   
   const db = mongoose.connection;
@@ -30,9 +35,7 @@ export const connectToMongo = async () => {
     console.warn('Disconnected from MongoDB');
   });
 
-  await mongoose.connect(
-    `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOST}:${MONGO_PORT}/${MONGO_AUTH_SOURCE}?authSource=${MONGO_AUTH_SOURCE}`,
-  )
+  await mongoose.connect(MONGO_CONNECTION_STRING);
 
   return db;
 }
